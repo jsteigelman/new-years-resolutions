69b00b464b27c804f94aae0c76450291b5e1b251
Joey Steigelman <joey.steigelman@gmail.com>
2022-01-09 19:18:56 -0500

    user can input a resolution to add to list

--- App.js
+++ App.js
@@ -1,13 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native'
 
 export default function App() {
+  const [inputResolution, setInputResolution] = useState('')
+  const [resolutions, setResolutions] = useState([])
+
+  const resolutionInputHandler = (input) => {
+    setInputResolution(input)
+  }
+
+  const addResolutionHandler = () => {
+    console.log('The entered resolution is: ' + inputResolution)
+    setResolutions(currentResolutions => [...currentResolutions, inputResolution])
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder='Enter your resolution'
+          style={styles.textInput}
+          onChangeText={resolutionInputHandler}
+          value={inputResolution}
+        />
+        <Button title='Add' onPress={addResolutionHandler} />
+      </View>
+
+      <View>
+        {resolutions.map((resolutionElement) => <Text key={resolutionElement} >{resolutionElement}</Text>)}
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +41,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  textInput: {
+    width: '80%',
+    borderBottomColor: 'black',
+    borderWidth: 1,
+    padding: 10,
+  },
+})
